aa0b48af86081b826672fe8982356eeaed0ede39
Dominik <dominik.olechny_priv@outlook.com>
2022-11-06 13:52:50 +0100

    test

--- lab4.py
+++ lab4.py
@@ -1,3 +1,6 @@
 Lzakupow = ["mleko", "chleb", "banan", "smietana"]
+Lzakupow[2] = "margaryna"
+print(Lzakupow[2])
 print(Lzakupow)
 print(Lzakupow[0], Lzakupow[2])
+print(Lzakupow[-1], Lzakupow[-2])
