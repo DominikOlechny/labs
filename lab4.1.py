aee695384aa7c0d61fe08fb15df336e53d2b2f93
Dominik <dominik.olechny_priv@outlook.com>
2022-11-06 14:03:18 +0100

    test

--- lab4.1.py
+++ lab4.1.py
@@ -1,3 +1,20 @@
 import random
-x = random(1,10)
-print(x)
+x = int(input("podaj liczbe elemtow"))
+lista1 = []
+for i in range(x):
+        n = random.randint(1, 10)
+        lista1.append(n)
+print(lista1)
+x2 = int(input("podaj liczbe elemtow"))
+lista2 = []
+for i in range(x2):
+        n2 = random.randint(5, 15)
+        lista2.append(n2)
+print(lista2)
+x3 = int(input("podaj licz"))
+if x3 in lista1:
+    print("wystepuje")
+elif x3 in lista2:
+    print("liczb wystepuje w lista2")
+else:
+    print("nie ma w obu")
